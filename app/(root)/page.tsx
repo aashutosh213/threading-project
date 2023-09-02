5c630681cd60b186e01025c56546b5d21ba320a2
Aashutosh Tiwari <64861698+aashutosh213@users.noreply.github.com>
2023-09-02 22:19:38 +0000

    Added user button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
-   <main>
-      <h1>Threads</h1>
-   </main>
+  //  <main>
+  //     <h1>Threads</h1>
+  //  </main>
+  <div>
+    <UserButton afterSignOutUrl="/" />
+  </div>
   )
 }
